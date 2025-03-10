e535ecbe82b1db8be84bb73e14a1011b1614a1db
ATiamrat <arnuwat.t@laundry-you.com>
2025-03-10 13:49:24 +0700

    fix code with chat gpt

--- android/src/main/java/hr/prijavituriste/in_app_comments/InAppCommentsPlugin.java
+++ android/src/main/java/hr/prijavituriste/in_app_comments/InAppCommentsPlugin.java
@@ -8,6 +8,10 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.OnLifecycleEvent;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
@@ -41,7 +45,7 @@ public class InAppCommentsPlugin implements FlutterPlugin, MethodCallHandler, Ac
             Intent intent = new Intent("com.huawei.appmarket.intent.action.guidecomment");
             intent.setPackage("com.huawei.appmarket");
 
-            // Start the activity using the new ActivityResultLauncher
+            // Start the activity using the registered launcher
             activityResultLauncher.launch(intent);
         } else {
             result.notImplemented();
@@ -57,33 +61,43 @@ public class InAppCommentsPlugin implements FlutterPlugin, MethodCallHandler, Ac
     public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
         activity = activityPluginBinding.getActivity();
 
-        // Register the activity result launcher
-        activityResultLauncher = activity.registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult resultData) {
-                        if (result == null) return;
-                        int resultCode = resultData.getResultCode();
-                        if (resultCode == 102 || resultCode == 103) {
-                            result.success(resultCode);
-                        } else {
-                            String errorMsg;
-                            switch (resultCode) {
-                                case 0: errorMsg = "Unknown error"; break;
-                                case 101: errorMsg = "The app has not been released on AppGallery"; break;
-                                case 104: errorMsg = "The HUAWEI ID sign-in status is invalid"; break;
-                                case 105: errorMsg = "User does not meet conditions for commenting"; break;
-                                case 106: errorMsg = "Commenting function is disabled"; break;
-                                case 107: errorMsg = "In-app commenting service is not supported"; break;
-                                case 108: errorMsg = "User canceled the comment"; break;
-                                default: errorMsg = "Unexpected error: " + resultCode;
+        // Ensure the Activity is a LifecycleOwner
+        if (activity instanceof LifecycleOwner) {
+            LifecycleOwner lifecycleOwner = (LifecycleOwner) activity;
+            lifecycleOwner.getLifecycle().addObserver(new LifecycleObserver() {
+                @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+                public void onCreate() {
+                    activityResultLauncher = activityPluginBinding.getActivity().registerForActivityResult(
+                            new ActivityResultContracts.StartActivityForResult(),
+                            new ActivityResultCallback<ActivityResult>() {
+                                @Override
+                                public void onActivityResult(ActivityResult resultData) {
+                                    if (result == null) return;
+                                    int resultCode = resultData.getResultCode();
+                                    if (resultCode == 102 || resultCode == 103) {
+                                        result.success(resultCode);
+                                    } else {
+                                        String errorMsg;
+                                        switch (resultCode) {
+                                            case 0: errorMsg = "Unknown error"; break;
+                                            case 101: errorMsg = "The app has not been released on AppGallery"; break;
+                                            case 104: errorMsg = "The HUAWEI ID sign-in status is invalid"; break;
+                                            case 105: errorMsg = "User does not meet conditions for commenting"; break;
+                                            case 106: errorMsg = "Commenting function is disabled"; break;
+                                            case 107: errorMsg = "In-app commenting service is not supported"; break;
+                                            case 108: errorMsg = "User canceled the comment"; break;
+                                            default: errorMsg = "Unexpected error: " + resultCode;
+                                        }
+                                        result.error(String.valueOf(resultCode), "error", errorMsg);
+                                    }
+                                }
                             }
-                            result.error(String.valueOf(resultCode), "error", errorMsg);
-                        }
-                    }
+                    );
                 }
-        );
+            });
+        } else {
+            Log.e("InAppCommentsPlugin", "Activity does not implement LifecycleOwner, unable to register for activity results.");
+        }
     }
 
     @Override
@@ -93,7 +107,7 @@ public class InAppCommentsPlugin implements FlutterPlugin, MethodCallHandler, Ac
 
     @Override
     public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
-        activity = activityPluginBinding.getActivity();
+        onAttachedToActivity(activityPluginBinding);
     }
 
     @Override
